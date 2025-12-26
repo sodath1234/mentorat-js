3df17482aff8ba27918d2f9ee3f504e1a69bd133
sodath1234 <pessesodath@gmail.com>
2025-12-26 13:16:45 +0100

    Premier commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Projet de Gestion des Stocks
+
+## Description
+Ce projet est une application développée dans le cadre des cours à l'IUT de l'Université de Parakou.
+
+## Objectifs
+- Gérer les produits
+- Suivre les entrées et sorties
+- Générer des rapports
+
+## Technologies utilisées
+- HTML
+- Javascript
+- CSS
+
+## Auteur
+Sodath PESSE
---
+++ script.js
@@ -0,0 +1,39 @@
+console.log("Mentorat JS Ok")
+
+
+
+
+
+
+
+
+/*hero*/
+.hero{
+    background:linear-gradient(to bottom, rgb(231, 229, 231),rgb(214, 210, 210));
+    color: white;
+    text-align: center;
+    padding: 1rem,1rem;
+}
+.hero h1{
+    font: size: 10px;
+    margin-bottom: 2rem;
+    text-transform: uppercase;
+}
+.hero p{
+    max-width: 700px;
+    margin-top:auto 3rem;
+    background: linear-gradient(to left);
+}
+.hero img{
+    width: 200px;
+    margin-top: 3rem;
+}
+.btn {
+   background: #740c33;
+      color: white;
+      padding: 1rem 1rem;
+      border: none;
+      border-radius: 15px;
+      font-weight: bold;
+      cursor: pointer;
+}
